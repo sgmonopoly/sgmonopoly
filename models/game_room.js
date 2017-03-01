986e3984b968aaf748aa320e2c83176bbf9e2b17
Stephen <yuanxiang@chinac.com>
2017-03-01 16:30:22 +0800

    房间对象，对象是否正在游戏中的属性

--- models/game_room.js
+++ models/game_room.js
@@ -3,11 +3,12 @@
  */
 'use strict';
 class GAME_Room {
-    constructor(_roomName, _users, _currentNum, _maxNum = 4) {
+    constructor(_roomName, _users, _currentNum, _maxNum = 4, _isGaming = false) {
         this._roomName = _roomName;
         this._currentNum = _currentNum;
         this._maxNum = _maxNum;
         this._users = _users;
+        this._isGaming = _isGaming;
     }
 }
 module.exports = GAME_Room;
--- models/sg_user.js
+++ models/sg_user.js
@@ -3,17 +3,18 @@
  */
 'use strict';
 class SG_User{
-    constructor(_money = 18000,_troop = 2000){
-        this._userId = "";//用户ID,socketId
-        this._nickname = "";//昵称
-        this._avatar = "";//头像
+    constructor(_userId, _nickname, _avatar, _money = 18000,_troop = 2000){
+        this._userId = _userId;//用户ID,socketId
+        this._nickname = _nickname;//昵称
+        this._avatar = _avatar;//头像
         this._money = _money;//默认金额18000
         this._troop = _troop;//默认兵力2000
         this._citys = [];//占有的城池
-        this._cityCount = 0;//城池数
+        this._cityCount = this._citys.length;//城池数
         this._heroCount = 0;//武将数
         this._order = 0;//顺序(1,2,3,4)
         this._suspended = 0;//0不暂停,其他数字为暂停轮数
+        this._status = 1;//状态 1为准备,2已准备,3开战,4掉线
     }
 }
 module.exports = SG_User;
