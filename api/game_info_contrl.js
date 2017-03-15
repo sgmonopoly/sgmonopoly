eb12a0320f25e2b8d1ef54d6d12d71e5d2ec7b57
Stephen <yuanxiang@chinac.com>
2017-03-15 16:44:57 +0800

    锦囊妙计和紧急军情API

--- api/game_info_contrl.js
+++ api/game_info_contrl.js
@@ -4,6 +4,8 @@
 'use strict';
 const mapInfo = require("../models/map_info");
 const heroInfo = require("../models/card_info");
+const situationInfo = require("../models/situation_info");
+const suggestionInfo = require("../models/suggestion_info");
 /**
  * 基本的地图信息
  * @param req
@@ -13,10 +15,28 @@ exports.mapInfo = (req, res) => {
     res.send(mapInfo);
 };
 /**
- * 基本的人物信息
+ * 基本的卡片信息
  * @param req
  * @param res
  */
-exports.heroInfo = (req, res) => {
+exports.cardInfo = (req, res) => {
     res.send(heroInfo);
+};
+
+/**
+ * 基本的紧急军情信息
+ * @param req
+ * @param res
+ */
+exports.situationInfo = (req, res) => {
+    res.send(situationInfo);
+};
+
+/**
+ * 基本的锦囊妙计信息
+ * @param req
+ * @param res
+ */
+exports.suggestionInfo = (req, res) => {
+    res.send(suggestionInfo);
 };
