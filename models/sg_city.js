1c3703b02a3356c7a647fffd375e87fd4b490677
Stephen <yuanxiang@chinac.com>
2017-03-17 17:07:33 +0800

    修改城市对象，去掉一些动态的属性，只保留原始地不会变地属性，动态属性抽象到游戏对象中

--- models/sg_city.js
+++ models/sg_city.js
@@ -11,42 +11,9 @@ class SG_City extends require('./sg_stage') {
         this.occupyPrice = _occupyPrice;//占领地价格
         this.buildPrice = _buildPrice;//建城费
         this.colorFollow = _colorFollow;//相同颜色区分(1~5,6是古战场)
-        this.cityType = _cityType;//1普通,2小城,4大城
-        this.toll = 0;//过路费(调用getToll来返回这个值)
         this.tax1 = this.occupyPrice / 2;//基数为领地价格的一半,普通为基数,小城为基数2倍,大城为基数4倍
         this.tax2 = this.occupyPrice;
         this.tax3 = this.occupyPrice * 2;
-        this.getToll(sameColorCityCount);
     }
-
-    /**
-     * 根据相同颜色的城池数量,返回过路费
-     * @param sameColorCityCount
-     * @returns {number|*}
-     */
-    getToll(sameColorCityCount = 1) {
-        if (this.colorFollow === sg_constant.city_follow.ancient) {
-            //古战场,每多一个相同颜色,价格根据500翻倍
-            this._toll = 500 * Math.pow(2, sameColorCityCount - 1);
-        } else {
-            switch (this.cityType) {
-                case (sg_constant.city_type.normal) :
-                    this._toll = this.tax1;
-                    break;
-                case (sg_constant.city_type.small) :
-                    this._toll = this.tax2;
-                    break;
-                case (sg_constant.city_type.big) :
-                    this._toll = this.tax3;
-                    break;
-            }
-            if (sameColorCityCount === 4) {
-                //一般城池,要全部4个相同颜色,才翻倍
-                this._toll = this._toll * 2;
-            }
-        }
-        return this._toll;
-    }
-
 }
 module.exports = SG_City;
